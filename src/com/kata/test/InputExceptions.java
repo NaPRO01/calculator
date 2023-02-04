0871a91dbd6cb1f0cb025c59e521ef6d92fcd76c
Kiril Vasilyeu <cool.kirill.vasiljev.01@yandex.ru>
2023-02-04 20:21:22 +0300

    Calc V2

--- src/com/kata/test/InputExceptions.java
+++ src/com/kata/test/InputExceptions.java
@@ -1,4 +1,24 @@
 package com.kata.test;
 
 public class InputExceptions extends Exception{
+
+    public InputExceptions() {
+    }
+
+    public InputExceptions(String message) {
+        super(message);
+    }
+
+    public InputExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InputExceptions(Throwable cause) {
+        super(cause);
+    }
+
+    public InputExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
--- src/com/kata/test/Solution.java
+++ src/com/kata/test/Solution.java
@@ -1,91 +1,17 @@
 package com.kata.test;
 
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution {
-
-    public static void main(String[] args) throws InputExceptions {
-
-        numberConverter converter = new numberConverter();
-        String[] action = {"+", "-", "/", "*"};
-        String[] splitterAction = {"\\+", "-", "/", "\\*"};
+    public static void main(String[] args) {
+        System.out.print( "Введите операцию в формате [a + b]: " );
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите выражение в одну строку: [7+8] + Enter");
-        String expression = scan.nextLine();
-
-        int actIndex = -1;
-        for (int i = 0; i < action.length; i++) {
-            if (expression.contains(action[i])){
-                actIndex = i;
-                break;
-            }
+        try{
+            System.out.println( Calculator.calc(scan.nextLine()) );
+        }catch (InputExceptions e){
+            System.err.println(e.getMessage());
         }
-
-        if (actIndex == -1){
-            System.out.println("Неверное выражение");
-            return;
-        }
-
-        String[] nums = expression.split(splitterAction[actIndex]);
-        if(nums.length > 2){
-            try {
-                throw new InputExceptions();
-            } catch (InputExceptions e){
-                System.out.println("Неверный формат ввода данных");
-                return;
-            }
-        }
-
-        if (converter.isRoman(nums[0]) == converter.isRoman(nums[1])){
-            int num1, num2;
-
-            boolean isRoman = converter.isRoman(nums[0]);
-            if (isRoman){
-                num1 = converter.toInt(nums[0]);
-                num2 = converter.toInt(nums[1]);
-            }
-            else {
-                num1 = Integer.parseInt(nums[0]);
-                num2 = Integer.parseInt(nums[1]);
-            }
-
-            int result = 0;
-            switch (action[actIndex]){
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    try {
-                        result = num1 / num2;
-                    } catch (ArithmeticException | InputMismatchException e) {
-                        System.out.println("Исключение : " + e);
-                        System.out.println("Only integer non-zero parameters allowed");
-
-                        break;
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("Не верный знак операции");
-            }
-
-            if (isRoman) {
-                System.out.println(converter.toRoman(result));
-            }
-            else
-                System.out.println(result);
-
-        }else{
-            System.out.println("Ожидаются числа в одном формате");
-        }
-
     }
-
 }
+
+
